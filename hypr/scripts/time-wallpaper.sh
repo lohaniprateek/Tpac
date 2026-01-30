c05e14ef9466c1d144790c6676120ee22008a2f2
Prateek Kumar <hlw.prateek@gmail.com>
2026-01-30 13:43:08 +0530

    Added: Wallpapers

--- hypr/scripts/time-wallpaper.sh
+++ hypr/scripts/time-wallpaper.sh
@@ -4,28 +4,56 @@ export XDG_RUNTIME_DIR=/run/user/$(id -u)
 export WAYLAND_DISPLAY=wayland-1
 
 HOUR=$(date +%-H)
-WALLDIR="$HOME/Tpac/wallpapers/time"
+WALLDIR="$HOME/Tpac/wallpapers/earth"
 
-if ((HOUR >= 0 && HOUR < 4)); then
+if ((HOUR >= 0 && HOUR < 1)); then
   WALL="$WALLDIR/0.png"
-elif ((HOUR >= 4 && HOUR < 6)); then
+elif ((HOUR >= 1 && HOUR < 2)); then
   WALL="$WALLDIR/1.png"
-elif ((HOUR >= 6 && HOUR < 8)); then
+elif ((HOUR >= 2 && HOUR < 3)); then
   WALL="$WALLDIR/2.png"
-elif ((HOUR >= 8 && HOUR < 12)); then
+elif ((HOUR >= 3 && HOUR < 4)); then
   WALL="$WALLDIR/3.png"
-elif ((HOUR >= 12 && HOUR < 15)); then
+elif ((HOUR >= 4 && HOUR < 5)); then
   WALL="$WALLDIR/4.png"
-elif ((HOUR >= 15 && HOUR < 17)); then
+elif ((HOUR >= 5 && HOUR < 6)); then
   WALL="$WALLDIR/5.png"
-elif ((HOUR >= 17 && HOUR < 18)); then
+elif ((HOUR >= 6 && HOUR < 7)); then
   WALL="$WALLDIR/6.png"
-elif ((HOUR >= 18 && HOUR < 19)); then
+elif ((HOUR >= 7 && HOUR < 8)); then
   WALL="$WALLDIR/7.png"
-elif ((HOUR >= 19 && HOUR < 20)); then
+elif ((HOUR >= 8 && HOUR < 9)); then
   WALL="$WALLDIR/8.png"
-else
+elif ((HOUR >= 9 && HOUR < 10)); then
   WALL="$WALLDIR/9.png"
+elif ((HOUR >= 10 && HOUR < 11)); then
+  WALL="$WALLDIR/10.png"
+elif ((HOUR >= 11 && HOUR < 12)); then
+  WALL="$WALLDIR/11.png"
+elif ((HOUR >= 12 && HOUR < 13)); then
+  WALL="$WALLDIR/12.png"
+elif ((HOUR >= 13 && HOUR < 14)); then
+  WALL="$WALLDIR/13.png"
+elif ((HOUR >= 14 && HOUR < 15)); then
+  WALL="$WALLDIR/14.png"
+elif ((HOUR >= 15 && HOUR < 16)); then
+  WALL="$WALLDIR/15.png"
+elif ((HOUR >= 16 && HOUR < 17)); then
+  WALL="$WALLDIR/16.png"
+elif ((HOUR >= 17 && HOUR < 18)); then
+  WALL="$WALLDIR/17.png"
+elif ((HOUR >= 18 && HOUR < 19)); then
+  WALL="$WALLDIR/18.png"
+elif ((HOUR >= 19 && HOUR < 20)); then
+  WALL="$WALLDIR/19.png"
+elif ((HOUR >= 20 && HOUR < 21)); then
+  WALL="$WALLDIR/20.png"
+elif ((HOUR >= 21 && HOUR < 22)); then
+  WALL="$WALLDIR/21.png"
+elif ((HOUR >= 22 && HOUR < 23)); then
+  WALL="$WALLDIR/22.png"
+else
+  WALL="$WALLDIR/23.png"
 fi
 
 swww img "$WALL" \
