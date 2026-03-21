33268030c5aef53bbf2062d07ec3055dfff0fb31
Prateek Kumar <hlw.prateek@gmail.com>
2026-03-21 21:24:23 +0530

    deleted junk

--- scripts/fehbg.sh
+++ scripts/fehbg.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-<<<<<<< HEAD
-feh --no-feh --bg-fill "$HOME/Tpac/wallpapers/ArchlinuxWallpaper.jpg"
-=======
+
 feh --no-feh --bg-fill '/home/prateek/Tpac/wallpapers/gruvyArch.png'
->>>>>>> 8af69cc (I am still learing  about life (zshrc and zprofile splitted))
-=======
-feh --no-feh --bg-fill '/home/prateek/Tpac/wallpapers/gruvyArch.png'
->>>>>>> feats
